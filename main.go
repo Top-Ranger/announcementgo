ec8c3d75a970b2c73625c0d5bc6f9cc4e8ba95d4
Marcus Soll <git@msoll.eu>
2020-12-12 22:22:53 +0100

    Use buffered channel for signal.Notify
    As recommended by doc, else signals could be lost

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func main() {
 
 	server.RunServer()
 
-	s := make(chan os.Signal)
+	s := make(chan os.Signal, 1)
 	signal.Notify(s, os.Interrupt, syscall.SIGTERM)
 
 	log.Println("main: waiting")
