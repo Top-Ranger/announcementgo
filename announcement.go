91f703ed67a9704e8eaec66ac2c494091d0ea7dd
Marcus Soll <git@msoll.eu>
2022-03-12 20:45:44 +0100

    Removed dead code

--- announcement.go
+++ announcement.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020,2021 Marcus Soll
+// Copyright 2020,2021,2022 Marcus Soll
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
